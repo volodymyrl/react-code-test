6150b2c4d3dd1ae9eb09917e26cc0c20e817d4a3
Volodymyr Lysytsia <vlysytsia@gmail.com>
2020-02-17 14:01:45 +0200

    create loader component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Loader } from "./components/Loader";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Loader />
+    </div>
+  );
 }
 
 export default App;
